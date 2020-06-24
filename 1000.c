297fd7082de1fb5c7199d300b689c72b409ad917
shuniquely <shuniquely@gmail.com>
2020-06-24 22:59:07 +0900

    Revise Baekjoon problem 1000.c

--- 1000.c
+++ 1000.c
@@ -1,13 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int sum(int, int);
+
 int main()
 {
 	int a, b;
 	int s;
 	scanf("%d%d", &a, &b);
-	s = a + b;
-	printf("%d", s);
+	printf("%d", sum(a, b));
 
 	return 0;
+}
+
+int sum(int a, int b)
+{
+	return a + b;
 }
