c32986abea0e5611d4bd2e630620ba1886c92e5a
shuniquely <shuniquely@gmail.com>
2020-06-30 23:35:39 +0900

    Revice Baekjoon problem 8393.c

--- 8393.c
+++ 8393.c
@@ -1,16 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int add(int);
+
 int main()
 {
-	int n, s = 0;
+	int n;
 
 	scanf("%d", &n);
+
+	printf("%d", add(n));
+}
+
+int add(int n)
+{
+	int s = 0;
 
 	for (int i = 1; i <= n; i++)
 	{
 		s += i;
 	}
 
-	printf("%d", s);
+	return s;
 }
