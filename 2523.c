715bbd4f47c14991822926fb76c882d8c4c398b5
shuniquely <shuniquely@gmail.com>
2020-07-01 14:46:28 +0900

    Revice 2523.c

--- 2523.c
+++ 2523.c
@@ -1,15 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+void stars(int);
+
 int main()
 {
-	int N, C, i;
+	int N;
 
 	scanf("%d", &N);
 
-	for (i = 1; i <= N * 2; i++)
+	stars(N);
+}
+
+void stars(int N)
+{
+	int C;
+
+	for (int i = 1; i <= N*2 -1; i++)
 	{
-		if (i > N) C = N - (i - N);
+		if (i > N) C = N*2 - i;
 		else C = i;
 
 		for (int j = 1; j <=C; j++)
