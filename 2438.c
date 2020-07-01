28041c8c1cdfba299d92df1a2c269445a5d260fa
shuniquely <shuniquely@gmail.com>
2020-07-01 14:46:06 +0900

    Revice 2438.c

--- 2438.c
+++ 2438.c
@@ -1,11 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+void stars (int);
+
 int main()
 {
-	int n, i, j;
+	int n;
 	scanf("%d", &n);
 
+	stars(n);
+}
+
+void stars(int n)
+{
+	int i, j;
 	i = 0;
 	while (i < n)
 	{
