d493d2ccfc86ddcc6bc950e32d4e97b42beb216f
shuniquely <shuniquely@gmail.com>
2020-07-01 14:45:46 +0900

    Revice 1676.c

--- 1676.c
+++ 1676.c
@@ -1,18 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int count_zero(int);
+
 int main()
 {
 	int N;
-	int result = 0;
-	int i = 5;
 	scanf("%d", &N);
+
+	printf("%d", count_zero(N));
+}
+
+int count_zero(int N)
+{
+	int result = 0, i =5;
 
 	while (i <= N)
 	{
 		result += N / i;
 		i *= 5;
 	}
-
-	printf("%d", result);
+	
+	return result;
 }
