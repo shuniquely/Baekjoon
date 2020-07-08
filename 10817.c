02b39e3d57a5c3b87ab1cfa7117bc00ed8bc1207
shuniquely <shuniquely@gmail.com>
2020-07-08 23:58:31 +0900

    Add 10817.c

---
+++ 10817.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int Max(int, int, int);
+
+int main()
+{
+    int A, B, C;
+
+    scanf("%d %d %d", &A, &B, &C);
+
+    printf("%d", Max(A, B, C));
+}
+
+int Max(int a, int b, int c)
+{
+    if (a > b)
+    {
+        if (a > c) return a;
+        else return c;
+    }
+    else
+    {
+        if (b > c) return b;
+        else return c;
+    }
+    
+}
