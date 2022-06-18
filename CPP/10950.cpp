46efcbf641eb146aaf31d292b9946339f20374de
Toram <shuniquely@gmail.com>
2022-06-18 23:45:12 +0900

    [Add] Problem solved with CPP
    CPP solutions

---
+++ CPP/10950.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int test;
+
+	cin >> test;
+
+	for (int i = 1; i <= test; i++)
+	{
+		int A, B;
+
+		cin >> A >> B;
+
+		cout << A + B << endl;;
+	}
+}
