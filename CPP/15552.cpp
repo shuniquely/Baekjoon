c3c1922162959ffac95bb8bac008ca8ba3b451c8
Toram <shuniquely@gmail.com>
2022-06-22 23:11:22 +0900

    [Add] Problem solved with CPP
    CPP solutions

---
+++ CPP/15552.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int T, A, B;
+
+	cin >> T;
+
+	for (int i = 1; i <= T; i++)
+	{
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+}
