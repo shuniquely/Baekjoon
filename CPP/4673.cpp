0edfa5b4ecb6ab3391ad1d31885de21285699683
Toram <shuniquely@gmail.com>
2022-08-04 23:56:41 +0900

    [Add] 4673.cpp
    CPP solutions

---
+++ CPP/4673.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintSelfNum();
+
+int main()
+{
+	PrintSelfNum;
+}
+
+void PrintSelfNum()
+{
+	// 셀프 넘버이면 0, 아니면 1로 나타낼 크기가 10000인 배열을 만든다.
+	const int MAX = 10000;
+	int arr[MAX] = {1};
+	int result = 2, i = 2;
+
+	while (1)
+	{
+		if (result > MAX) break;
+
+		result = i +
+	}
+}
