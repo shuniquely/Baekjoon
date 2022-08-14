e20d8bdf1e2e098fe65547b5607db814feb6aafb
Toram <shuniquely@gmail.com>
2022-08-15 01:23:42 +0900

    [Update] 11720.cpp
    CPP solutions

--- CPP/11720.cpp
+++ CPP/11720.cpp
@@ -15,40 +15,8 @@ int main()
 
 	for (int i = 0; i < N; i++)
 	{
-		int ctoi = (int)arr[i];
-
-		switch (ctoi)
-		{
-		case 48:
-			break;
-		case 49:
-			result += 1;
-			break;
-		case 50:
-			result += 2;
-			break;
-		case 51:
-			result += 3;
-			break;
-		case 52:
-			result += 4;
-			break;
-		case 53:
-			result += 5;
-			break;
-		case 54:
-			result += 6;
-			break;
-		case 55:
-			result += 7;
-			break;
-		case 56:
-			result += 8;
-			break;
-		case 57:
-			result += 9;
-			break;
-		}
+		int ctoi = (int)arr[i] - 48;
+		result += ctoi;
 	}
 
 	cout << result << "\n";
